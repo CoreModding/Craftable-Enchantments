36d2e60cc1c6d4be636c0e1b4cb426c70691dab9
ShooShoSha <shooshosha@gmail.com>
2014-02-26 15:26:34 -0600

    Add proxies.
    Means of separating tasks by proxy.

---
+++ src/main/java/info/coremodding/craftenchants/proxy/ClientProxy.java
@@ -0,0 +1,13 @@
+/** 
+ * Craftable Enchantments
+ * ClientProxy.java
+ * //TODO File description
+ * @copyright (C) 2014
+ * @author Kevin O'Brien (AKA ShooShoSha)
+ * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public License</a>
+ */
+package info.coremodding.craftenchants.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+}
---
+++ src/main/java/info/coremodding/craftenchants/proxy/ServerProxy.java
@@ -0,0 +1,13 @@
+/** 
+ * Craftable Enchantments
+ * ServerProxy.java
+ * //TODO File description
+ * @copyright (C) 2014
+ * @author Kevin O'Brien (AKA ShooShoSha)
+ * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public License</a>
+ */
+package info.coremodding.craftenchants.proxy;
+
+public class ServerProxy extends CommonProxy {
+
+}
