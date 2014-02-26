ae4cfa27767999987808b2d81476110afce20241
ShooShoSha <shooshosha@gmail.com>
2014-02-26 15:28:51 -0600

    Add interface.
    Define behavior for all resources added to game.

---
+++ src/main/java/info/coremodding/craftenchants/item/RegistrableItem.java
@@ -0,0 +1,19 @@
+/** 
+  * Craftable Enchantments
+  * RegistrableItem.java
+  * //TODO File description
+  * @copyright (C) 2014
+  * @author Kevin O'Brien (AKA ShooShoSha)
+  * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public License</a>
+  */
+package info.coremodding.craftenchants.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public interface RegistrableItem {
+    void setName(String unlocalName);
+    void setTexture();
+    void setTab(CreativeTabs tab);
+    void addToGame();
+    void addRecipe();
+}
