0b70435e31b4767bf69def61a1e0d05a8965a1b6
ShooShoSha <shooshosha@gmail.com>
2014-02-26 15:27:03 -0600

    Add references.
    Contains mod-wide references.

---
+++ src/main/java/info/coremodding/craftenchants/library/Reference.java
@@ -0,0 +1,18 @@
+/** 
+ * Craftable Enchantments
+ * Reference.java
+ * //TODO File description
+ * @copyright (C) 2014
+ * @author Kevin O'Brien (AKA ShooShoSha)
+ * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public License</a>
+ */
+
+package info.coremodding.craftenchants.library;
+
+public final class Reference {
+    public static final String MOD_NAME = "Craftable Enchantments";
+    public static final String MOD_ID = "craftenchants";
+
+    public static final String CLIENT_PROXY = "info.coremodding.craftenchants.proxy.ClientProxy";
+    public static final String SERVER_PROXY = "info.coremodding.craftenchants.proxy.ServerProxy";
+}
