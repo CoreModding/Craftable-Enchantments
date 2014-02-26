d3d559742d67a4355832c8d0e11a7b913d5ac03e
ShooShoSha <shooshosha@gmail.com>
2014-02-26 15:29:26 -0600

    Add item.
    Defines an item that will add enchantment to a tool and is craftable.

---
+++ src/main/java/info/coremodding/craftenchants/item/SharpeningStone.java
@@ -0,0 +1,21 @@
+/** 
+  * Craftable Enchantments
+  * SharpeningStone.java
+  * //TODO File description
+  * @copyright (C) 2014
+  * @author Kevin O'Brien (AKA ShooShoSha)
+  * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public License</a>
+  */
+package info.coremodding.craftenchants.item;
+
+import net.minecraft.item.Item;
+
+import info.coremodding.craftenchants.proxy.CommonProxy;
+
+public class SharpeningStone extends Item {
+    public SharpeningStone() {
+	super();
+	setUnlocalizedName("sharpeningstone");
+	setCreativeTab(CommonProxy.tab);
+    }
+}
