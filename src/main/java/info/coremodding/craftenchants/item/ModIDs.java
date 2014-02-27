2d8e77ef68b4881c8338840f1d41b87b85fbbf92
ShooShoSha <shooshosha@gmail.com>
2014-02-27 15:52:56 -0600

    Add ids reference. Tracks all ids used for the items. May hold other ids if it becomes necessary.

---
+++ src/main/java/info/coremodding/craftenchants/item/ModIDs.java
@@ -0,0 +1,36 @@
+/** 
+  * Craftable-Enchantments
+  * ModIds.java
+  * //TODO File description
+  * @copyright (C) 2014
+  * @author Kevin O'Brien (AKA ShooShoSha)
+  * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public License</a>
+  */
+package info.coremodding.craftenchants.item;
+
+public class ModIDs {
+    
+    public static class Items {
+	public static final class Default {
+	    public static final int armorKit = 16161;
+	    public static final int blastResistPad = 16162;
+	    public static final int ironCounterweight = 16163;
+	    public static final int ironStuds = 16164;
+	    public static final int reinforcedNock = 16165;
+	    public static final int sharpeningStone = 16166;
+	    public static final int snorkle = 16167;
+	    public static final int stoneCounterweight = 16168;
+	    public static final int woolPadding = 16169;
+	}
+
+	// Current item IDs
+	public static int armorKit;
+	public static int blastResistPad;
+	public static int ironCounterweight;
+	public static int ironStuds;
+	public static int sharpeningStone;
+	public static int snorkle;
+	public static int stoneCounterweight;
+	public static int woolPadding;
+    }
+}
