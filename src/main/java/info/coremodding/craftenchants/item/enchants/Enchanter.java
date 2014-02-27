e5c832130850ccfb3bdde61f093e2468dc220d37
ShooShoSha <shooshosha@gmail.com>
2014-02-27 15:56:44 -0600

    Update interface. Interface for items that can be applied to other items.

---
+++ src/main/java/info/coremodding/craftenchants/item/enchants/Enchanter.java
@@ -0,0 +1,13 @@
+/** 
+  * Craftable-Enchantments
+  * Enchanter.java
+  * //TODO File description
+  * @copyright (C) 2014
+  * @author Kevin O'Brien (AKA ShooShoSha)
+  * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public License</a>
+  */
+package info.coremodding.craftenchants.item.enchants;
+
+public interface Enchanter {
+    void applyEnchant();
+}
