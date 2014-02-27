8edd89e16899c794c3ce50986212efa3c8a9df64
ShooShoSha <shooshosha@gmail.com>
2014-02-27 10:23:51 -0600

    Update recipes.
    Recipes refer to correct references now.

--- src/main/java/info/coremodding/craftenchants/item/CEItemRecipe.java
+++ src/main/java/info/coremodding/craftenchants/item/CEItemRecipe.java
@@ -8,8 +8,8 @@
   */
 package info.coremodding.craftenchants.item;
 
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class CEItemRecipe {
@@ -17,71 +17,71 @@ public class CEItemRecipe {
 	"FFF",
 	"FSF",
 	"FFF",
-	'F', new ItemStack(Items.flint),
-	'S', new ItemStack(Blocks.stone)
+	'F', new ItemStack(Item.flint),
+	'S', new ItemStack(Block.stone)
     };
     
     public static final Object[] armorKit = new Object[] {
 	"LLL", 
 	"LWL", 
 	"LLL",
-	'L', new ItemStack(Items.leather, 1),
-	'W', Blocks.wool
+	'L', new ItemStack(Item.leather, 1),
+	'W', Block.cloth
     };
     
     public static final Object[] woolPadding = new Object[] {
 	"   ",
 	"WRW",
 	"WRW",
-	'W', Blocks.wool,
-	'R', new ItemStack(Items.string)
+	'W', Block.cloth,
+	'R', new ItemStack(Item.silk)
     };
     
     public static final Object[] ironStuds = new Object[] {
 	"   ",
 	"I I",
 	"L L",
-	'I', new ItemStack(Items.iron_ingot),
-	'L', new ItemStack(Items.leather)
+	'I', new ItemStack(Item.ingotIron),
+	'L', new ItemStack(Item.leather)
     };
     
     public static final Object[] stoneCounterweight = new Object[] {
 	"WWW",
 	"WSW",
 	"WWW",
-	'W', Blocks.wool,
-	'S', Blocks.stone
+	'W', Block.cloth,
+	'S', Block.stone
     };
     
     public static final Object[] ironCounterweight = new Object[] {
 	"WWW",
 	"WIW",
 	"WWW",
-	'W', Blocks.wool,
-	'I', Blocks.iron_block
+	'W', Block.cloth,
+	'I', Block.blockIron
     };
     
     public static final Object[] blastResistPad = new Object[] {
 	"III",
 	"ISI",
 	"III",
-	'I', new ItemStack(Items.iron_ingot),
-	'S', Blocks.stone
+	'I', new ItemStack(Item.ingotIron),
+	'S', Block.stone
     };
     
     public static final Object[] snorkle = new Object[] {
 	"GGS",
 	"GGG",
 	"III",
-	'G', Blocks.glass,
-	'S', new ItemStack(Items.reeds),
-	'I', new ItemStack(Items.iron_ingot)
+	'G', Block.glass,
+	'S', new ItemStack(Item.reed),
+	'I', new ItemStack(Item.ingotIron)
     };
     
     public static final Object[] reinforcedNock = new Object[] {
 	"H  ",
 	" H ",
 	"  H",
-	'H', Items.melon
+	'H', Item.melon
     };
 }
