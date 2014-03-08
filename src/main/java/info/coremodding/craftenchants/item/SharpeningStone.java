eba7ba0976af5c5c202eb76caf4f8f730decd5b8
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-03-07 16:24:46 -0800

    None of those tags are allowed, and javadoc should be on the class itself

--- src/main/java/info/coremodding/craftenchants/item/SharpeningStone.java
+++ src/main/java/info/coremodding/craftenchants/item/SharpeningStone.java
@@ -1,13 +1,3 @@
-/**
- * Craftable Enchantments
- * SharpeningStone.java
- * //TODO File description
- * 
- * @copyright (C) 2014
- * @author Kevin O'Brien (AKA ShooShoSha)
- * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public
- *          License</a>
- */
 package info.coremodding.craftenchants.item;
 
 import info.coremodding.craftenchants.proxy.CommonProxy;
--- src/main/java/info/coremodding/craftenchants/library/Reference.java
+++ src/main/java/info/coremodding/craftenchants/library/Reference.java
@@ -1,14 +1,3 @@
-/**
- * Craftable Enchantments
- * Reference.java
- * //TODO File description
- * 
- * @copyright (C) 2014
- * @author Kevin O'Brien (AKA ShooShoSha)
- * @license <a href="http://www.gnu.org/licenses/">GNU Lesser General Public
- *          License</a>
- */
-
 package info.coremodding.craftenchants.library;
 
 /**
