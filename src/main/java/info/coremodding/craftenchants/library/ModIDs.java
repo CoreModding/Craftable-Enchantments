a0299d5b5687458abd9a7dbbc0c7f1e6ab83dd6b
ShooShoSha <shooshosha@gmail.com>
2014-03-18 10:34:08 -0500

    Add items. Add Fire Oil and Flame Resistant Cream as items.

--- src/main/java/info/coremodding/craftenchants/library/ModIDs.java
+++ src/main/java/info/coremodding/craftenchants/library/ModIDs.java
@@ -11,18 +11,22 @@ public class ModIDs {
         public static final class Default {
             public static final int armorKit = 16161;
             public static final int blastResistPad = 16162;
-            public static final int ironCounterweight = 16163;
-            public static final int ironStuds = 16164;
-            public static final int reinforcedNock = 16165;
-            public static final int sharpeningStone = 16166;
-            public static final int snorkle = 16167;
-            public static final int stoneCounterweight = 16168;
-            public static final int woolPadding = 16169;
+            public static final int fireOil = 16163;
+            public static final int flameResistantOil = 16164;
+            public static final int ironCounterweight = 16165;
+            public static final int ironStuds = 16166;
+            public static final int reinforcedNock = 16167;
+            public static final int sharpeningStone = 16168;
+            public static final int snorkle = 16169;
+            public static final int stoneCounterweight = 16170;
+            public static final int woolPadding = 16171;
         }
 
         // Current IDs
         public static int armorKit;
         public static int blastResistPad;
+        public static int fireOil;
+        public static int flameResistantOil;
         public static int ironCounterweight;
         public static int ironStuds;
         public static int sharpeningStone;
