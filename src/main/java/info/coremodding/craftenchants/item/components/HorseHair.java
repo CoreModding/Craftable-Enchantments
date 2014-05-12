5257ed0d598fb4c144fbfd4332daa71d5a426f30
ShooShoSha <shooshosha@gmail.com>
2014-05-12 09:46:26 -0500

    Increase stack size.

--- src/main/java/info/coremodding/craftenchants/item/components/HorseHair.java
+++ src/main/java/info/coremodding/craftenchants/item/components/HorseHair.java
@@ -1,6 +1,5 @@
 package info.coremodding.craftenchants.item.components;
 
-import net.minecraft.enchantment.Enchantment;
 import info.coremodding.craftenchants.item.ItemCE;
 
 public class HorseHair extends ItemCE {
@@ -8,7 +7,7 @@ public class HorseHair extends ItemCE {
 	public HorseHair(int id) {
 		super(id);
 		setName("horsehair");
-		setEnchant(Enchantment.power, 2);
+		setMaxStackSize(16);
 	}
 
 }
