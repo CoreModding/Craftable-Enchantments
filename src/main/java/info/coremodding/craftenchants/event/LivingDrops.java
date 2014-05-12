055bb69be02190818aedfb43e393f909f63563e5
ShooShoSha <shooshosha@gmail.com>
2014-05-12 09:44:54 -0500

    Rename class.

--- src/main/java/info/coremodding/craftenchants/event/LivingDrops.java
+++ src/main/java/info/coremodding/craftenchants/event/LivingDrops.java
@@ -8,7 +8,7 @@ import net.minecraft.entity.passive.EntityHorse;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
-public class LivingDropsEvent {
+public class LivingDrops {
 
 	@ForgeSubscribe
 	public void onEntityLivingDeath(LivingDeathEvent event) {
@@ -17,7 +17,7 @@ public class LivingDropsEvent {
 			Random random = new Random();
 			int dropChance = random.nextInt(100);
 			if(dropChance < 5) {
-			    int dropCount = random.nextInt(3);
+			    int dropCount = random.nextInt(3) + 1;
 			    event.entity.dropItem(ModItems.horseHair.itemID, dropCount);
 			}
 		}
