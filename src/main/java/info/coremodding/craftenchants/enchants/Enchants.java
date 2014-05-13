a8879dd3ba1ec13fd106fb2e9fc97885f0b3764f
ShooShoSha <shooshosha@gmail.com>
2014-05-13 08:44:37 -0500

    Create single reference class with inner classes.

---
+++ src/main/java/info/coremodding/craftenchants/enchants/Enchants.java
@@ -0,0 +1,98 @@
+/**
+  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
+  * http://www.gnu.org/licenses/gpl.html
+  * http://www.gnu.org/licenses/lgpl.html
+  */
+package info.coremodding.craftenchants.enchants;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraft.item.EnumToolMaterial;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+import info.coremodding.craftenchants.enchants.item.ArmorKit;
+import info.coremodding.craftenchants.enchants.item.BlastResistPad;
+import info.coremodding.craftenchants.enchants.item.Counterweight;
+import info.coremodding.craftenchants.enchants.item.ReinforcedNock;
+import info.coremodding.craftenchants.enchants.item.SharpeningStone;
+import info.coremodding.craftenchants.enchants.item.Snorkle;
+import info.coremodding.craftenchants.enchants.item.Studs;
+import info.coremodding.craftenchants.enchants.item.WoolPadding;
+import info.coremodding.craftenchants.enchants.item.component.HorseHair;
+import info.coremodding.craftenchants.enchants.potion.FireOil;
+import info.coremodding.craftenchants.enchants.potion.FlameResistantCream;
+import info.coremodding.craftenchants.library.ModIDs;
+
+public class Enchants {
+    public static class Items {
+        // Collection of all items
+        public static Set<ItemCE> items = new HashSet<>();
+        
+        /**
+         *  Instances of all items
+         */
+        public static ItemCE armorKit;
+        public static ItemCE blastResistPad;
+        
+        public static ItemCE ironCounterweight;
+        public static ItemCE ironStuds;
+        public static ItemCE reinforcedNock;
+        public static ItemCE sharpeningStone;
+        public static ItemCE snorkle;
+        public static ItemCE stoneCounterweight;
+        public static ItemCE woolPadding;
+        public static ItemCE horseHair;
+
+        public static void initialize() {
+            createItems();
+            registerItems();
+        }
+
+        private static void createItems() {
+            items.add(armorKit = new ArmorKit(ModIDs.Items.Default.armorKit));
+            items.add(blastResistPad = new BlastResistPad(ModIDs.Items.Default.blastResistPad));
+            items.add(ironCounterweight = new Counterweight(ModIDs.Items.Default.ironCounterweight, EnumToolMaterial.IRON));
+            items.add(ironStuds = new Studs(ModIDs.Items.Default.ironStuds, EnumToolMaterial.IRON));
+            items.add(reinforcedNock = new ReinforcedNock(ModIDs.Items.Default.reinforcedNock));
+            items.add(sharpeningStone = new SharpeningStone(ModIDs.Items.Default.sharpeningStone));
+            items.add(snorkle = new Snorkle(ModIDs.Items.Default.snorkle));
+            items.add(stoneCounterweight = new Counterweight(ModIDs.Items.Default.stoneCounterweight, EnumToolMaterial.STONE));
+            items.add(woolPadding = new WoolPadding(ModIDs.Items.Default.woolPadding));
+            items.add(horseHair = new HorseHair(ModIDs.Items.Default.horseHair));
+        }
+
+        private static void registerItems() {
+            for (ItemCE item : items) {
+                GameRegistry.registerItem(item, item.getUnlocalizedName());
+            }
+        }
+    }
+    
+    public static class Potions {
+        //Collection of all potions
+        public static Set<PotionCE> potions = new HashSet<>();
+        
+        //Instances of all potions
+        public static PotionCE fireOil;
+        public static PotionCE flameResistantCream;
+        
+        public static void initialize() {
+            createPotions();
+            registerPotions();
+        }
+        
+        private static void createPotions() {
+            potions.add(fireOil = new FireOil(ModIDs.Potions.Default.fireOil));
+            potions.add(flameResistantCream = new FlameResistantCream(ModIDs.Potions.Default.flameResistantCream));
+        }
+        
+        private static void registerPotions() {
+            
+        }
+    }
+    
+    private Enchants() {
+        //No construction of object
+    }
+}
