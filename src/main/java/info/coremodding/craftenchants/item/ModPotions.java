b71fa9cc11889df8d99bfafea74e996a305dcd06
ShooShoSha <shooshosha@gmail.com>
2014-05-13 08:24:13 -0500

    Create potions handler.

---
+++ src/main/java/info/coremodding/craftenchants/item/ModPotions.java
@@ -0,0 +1,36 @@
+/**
+  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
+  * http://www.gnu.org/licenses/gpl.html
+  * http://www.gnu.org/licenses/lgpl.html
+  */
+package info.coremodding.craftenchants.item;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import info.coremodding.craftenchants.item.enchants.FireOil;
+import info.coremodding.craftenchants.item.enchants.FlameResistantCream;
+import info.coremodding.craftenchants.library.ModIDs;
+
+public class ModPotions {
+    //Collection of all potions
+    public static Set<PotionCE> potions = new HashSet<>();
+    
+    //Instances of all potions
+    public static PotionCE fireOil;
+    public static PotionCE flameResistantCream;
+    
+    public static void initialize() {
+        createPotions();
+        registerPotions();
+    }
+    
+    private static void createPotions() {
+        potions.add(fireOil = new FireOil(ModIDs.Potions.Default.fireOil));
+        potions.add(flameResistantCream = new FlameResistantCream(ModIDs.Potions.Default.flameResistantCream));
+    }
+    
+    private static void registerPotions() {
+        
+    }
+}
