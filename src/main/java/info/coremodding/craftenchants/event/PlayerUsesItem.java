346047ea3565ac8c9866497b2f37eeec054816b7
ShooShoSha <shooshosha@gmail.com>
2014-05-27 08:45:50 -0500

    Return to square one.

--- src/main/java/info/coremodding/craftenchants/event/PlayerUsesItem.java
+++ src/main/java/info/coremodding/craftenchants/event/PlayerUsesItem.java
@@ -5,28 +5,7 @@
   */
 package info.coremodding.craftenchants.event;
 
-import java.util.Random;
-
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.EntityHorse;
-import net.minecraft.item.ItemShears;
-import net.minecraftforge.event.ForgeSubscribe;
-import net.minecraftforge.event.entity.player.EntityInteractEvent;
-
-import info.coremodding.craftenchants.item.ModItems;
 
 public class PlayerUsesItem {
-        
-    @ForgeSubscribe
-    public void onShearUse(EntityInteractEvent interaction) {
-        Entity target = interaction.target;
-        Entity player = interaction.entityPlayer;
-        if(target instanceof EntityHorse) {
-            if (player != null) {
-                Random random = new Random();
-                int dropCount = random.nextInt(6) + 1;
-                interaction.target.dropItem(ModItems.horseHair.itemID, dropCount);
-            }
-        }
-    }
+    
 }
