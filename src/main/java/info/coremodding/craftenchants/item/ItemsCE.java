47cdbbbe934f28b60d51ef565a1e0fb2261bc302
ShooShoSha <shooshosha@gmail.com>
2014-06-03 11:00:07 -0500

    Finish registering items to game.

--- src/main/java/info/coremodding/craftenchants/item/ItemsCE.java
+++ src/main/java/info/coremodding/craftenchants/item/ItemsCE.java
@@ -7,17 +7,38 @@ package info.coremodding.craftenchants.item;
 import java.util.HashSet;
 import java.util.Set;
 
+import net.minecraft.item.Item;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 import info.coremodding.craftenchants.item.components.HorseHair;
+import info.coremodding.craftenchants.item.enchants.ArmorKit;
+import info.coremodding.craftenchants.item.enchants.BlastResistPad;
+import info.coremodding.craftenchants.item.enchants.CounterWeight;
+import info.coremodding.craftenchants.item.enchants.FireOil;
+import info.coremodding.craftenchants.item.enchants.ReinforcedNock;
 import info.coremodding.craftenchants.item.enchants.SharpeningStone;
+import info.coremodding.craftenchants.item.enchants.Snorkle;
+import info.coremodding.craftenchants.item.enchants.Studs;
+import info.coremodding.craftenchants.item.enchants.WoolPadding;
 import info.coremodding.craftenchants.item.recipes.EnchantsItem;
 
 public class ItemsCE {
   public static Set<ItemCE> items = new HashSet<>();
 
+  /* Component items */
   public static ItemCE horseHair;
+  
+  /* Enchantment items */
+  public static ItemCE armorKit;
+  public static ItemCE blastResistPad;
+  public static ItemCE counterWeightIron;
+  public static ItemCE counterWeightStone;
+  public static ItemCE fireOil;
+  public static ItemCE reinforcedNock;
   public static ItemCE sharpeningStone;
+  public static ItemCE snorkle;
+  public static ItemCE studsIron;
+  public static ItemCE woolPadding;
 
   public static void initialize() {
     createItems();
@@ -27,7 +48,17 @@ public class ItemsCE {
 
   public static void createItems() {
     items.add(horseHair = new HorseHair());
+    
+    items.add(armorKit = new ArmorKit());
+    items.add(blastResistPad = new BlastResistPad());
+    items.add(counterWeightIron = new CounterWeight(Item.ToolMaterial.IRON));
+    items.add(counterWeightStone = new CounterWeight(Item.ToolMaterial.STONE));
+    items.add(fireOil = new FireOil());
+    items.add(reinforcedNock = new ReinforcedNock());
     items.add(sharpeningStone = new SharpeningStone());
+    items.add(snorkle = new Snorkle());
+    items.add(studsIron = new Studs(Item.ToolMaterial.IRON));
+    items.add(woolPadding = new WoolPadding());
   }
 
   public static void registerItems() {
