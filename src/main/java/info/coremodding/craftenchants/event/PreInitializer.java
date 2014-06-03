8b73d44fdd1fadcd599ec6e06c2a097db8ad3ecb
ShooShoSha <shooshosha@gmail.com>
2014-06-03 09:33:50 -0500

    Initialize potions.

--- src/main/java/info/coremodding/craftenchants/event/PreInitializer.java
+++ src/main/java/info/coremodding/craftenchants/event/PreInitializer.java
@@ -6,11 +6,15 @@ package info.coremodding.craftenchants.event;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+import info.coremodding.craftenchants.potion.PotionsCE;
+
 public final class PreInitializer {
 
   public static void handle(FMLPreInitializationEvent event) {
     // TODO configuration handler
     // TODO version handler
+    PotionsCE.initialize();
+    
   }
 
 }
