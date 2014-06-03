9367f0631773d3f03868e681798c7e3c56540ccc
ShooShoSha <shooshosha@gmail.com>
2014-06-03 10:59:49 -0500

    Finish adding enchantment recipes. Alphabetize variables (dat ocd).

--- src/main/java/info/coremodding/craftenchants/item/recipes/EnchantsItem.java
+++ src/main/java/info/coremodding/craftenchants/item/recipes/EnchantsItem.java
@@ -12,34 +12,48 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import info.coremodding.craftenchants.item.ItemsCE;
 
 public class EnchantsItem {
-  public static final Object[] sharpeningStone = new Object[] {"FFF", "FSF", "FFF", 'F',
-      new ItemStack(Items.flint), 'S', new ItemStack(Blocks.stone)};
-
+  /* Recipes for enchanting items */
   public static final Object[] armorKit = new Object[] {"LLL", "LWL", "LLL", 'L',
       new ItemStack(Items.leather, 1), 'W', Blocks.wool};
-
-  public static final Object[] woolPadding = new Object[] {"   ", "WRW", "WRW", 'W', Blocks.wool,
-      'R', new ItemStack(Items.string)};
-
-  public static final Object[] ironStuds = new Object[] {"   ", "I I", "L L", 'I',
-      new ItemStack(Items.iron_ingot), 'L', new ItemStack(Items.leather)};
-
-  public static final Object[] stoneCounterweight = new Object[] {"WWW", "WSW", "WWW", 'W',
-      Blocks.wool, 'S', Blocks.stone};
-
-  public static final Object[] ironCounterweight = new Object[] {"WWW", "WIW", "WWW", 'W',
-      Blocks.wool, 'I', Blocks.iron_block};
 
   public static final Object[] blastResistPad = new Object[] {"III", "ISI", "III", 'I',
       new ItemStack(Items.iron_ingot), 'S', Blocks.stone};
 
-  public static final Object[] snorkle = new Object[] {"GGS", "GGG", "III", 'G', Blocks.glass, 'S',
-      Items.reeds, 'I', Items.iron_ingot};
+  public static final Object[] counterWeightIron = new Object[] {"WWW", "WSW", "WWW", 'W',
+      Blocks.wool, 'S', Blocks.iron_block};
+
+  public static final Object[] counterWeightStone = new Object[] {"WWW", "WIW", "WWW", 'W',
+      Blocks.wool, 'I', Blocks.stone};
+
+  public static final Object[] fireOil = new Object[] {new ItemStack(Items.potionitem),
+      new ItemStack(Items.magma_cream)};
 
   public static final Object[] reinforcedNock = new Object[] {"H  ", " H ", "  H", 'H',
       ItemsCE.horseHair};
 
+  public static final Object[] sharpeningStone = new Object[] {"FFF", "FSF", "FFF", 'F',
+      new ItemStack(Items.flint), 'S', new ItemStack(Blocks.stone)};
+
+  public static final Object[] snorkle = new Object[] {"GGS", "GGG", "III", 'G', Blocks.glass, 'S',
+      Items.reeds, 'I', Items.iron_ingot};
+
+  public static final Object[] studsIron = new Object[] {"   ", "I I", "L L", 'I',
+      new ItemStack(Items.iron_ingot), 'L', new ItemStack(Items.leather)};
+
+  public static final Object[] woolPadding = new Object[] {"   ", "WRW", "WRW", 'W', Blocks.wool,
+      'R', new ItemStack(Items.string)};
+
   public static void addRecipes() {
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.armorKit), armorKit);
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.blastResistPad), blastResistPad);
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.counterWeightIron), counterWeightIron);
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.counterWeightStone), counterWeightStone);
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.reinforcedNock), reinforcedNock);    
     GameRegistry.addRecipe(new ItemStack(ItemsCE.sharpeningStone), sharpeningStone);
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.snorkle), snorkle);
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.studsIron), studsIron);
+    GameRegistry.addRecipe(new ItemStack(ItemsCE.woolPadding), woolPadding);
+
+    GameRegistry.addShapelessRecipe(new ItemStack(ItemsCE.fireOil), fireOil);
   }
 }
