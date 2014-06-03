33230fec60693712c8c3dfe7b5ae0909cb4f31bb
ShooShoSha <shooshosha@gmail.com>
2014-06-03 08:46:24 -0500

    Move item initialization.

--- src/main/java/info/coremodding/craftenchants/event/PreInitializer.java
+++ src/main/java/info/coremodding/craftenchants/event/PreInitializer.java
@@ -1,18 +1,16 @@
 /**
  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
- * http://www.gnu.org/licenses/gpl.html
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/gpl.html http://www.gnu.org/licenses/lgpl.html
  */
 package info.coremodding.craftenchants.event;
-
-import info.coremodding.craftenchants.item.ItemsCE;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public final class PreInitializer {
-    
-    public static void handle(FMLPreInitializationEvent event) {
-        ItemsCE.initialize();
-    }
-    
+
+  public static void handle(FMLPreInitializationEvent event) {
+    // TODO configuration handler
+    // TODO version handler
+  }
+
 }
