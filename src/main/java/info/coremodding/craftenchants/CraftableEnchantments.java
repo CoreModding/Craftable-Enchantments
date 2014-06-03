e2c3c0b17923bf6115338088e289ea647a4ff806
ShooShoSha <shooshosha@gmail.com>
2014-06-03 08:45:49 -0500

    Add initializers methods and classes.

--- src/main/java/info/coremodding/craftenchants/CraftableEnchantments.java
+++ src/main/java/info/coremodding/craftenchants/CraftableEnchantments.java
@@ -1,7 +1,6 @@
 /**
  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
- * http://www.gnu.org/licenses/gpl.html
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/gpl.html http://www.gnu.org/licenses/lgpl.html
  */
 package info.coremodding.craftenchants;
 
@@ -9,24 +8,38 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+import info.coremodding.craftenchants.event.Initializer;
+import info.coremodding.craftenchants.event.PostInitializer;
 import info.coremodding.craftenchants.event.PreInitializer;
 import info.coremodding.craftenchants.proxy.Proxy;
 
 @Mod(modid = CraftableEnchantments.ID, name = CraftableEnchantments.NAME)
 public class CraftableEnchantments {
-    public static final String NAME = "Craftable Enchantments";
-    public static final String ID = "craftenchants";
-    
-    @Instance(ID)
-    public static CraftableEnchantments instance;
-    
-    @SidedProxy(clientSide = Proxy.CLIENT, serverSide = Proxy.SERVER)
-    public static Proxy proxy;
-    
-    @EventHandler
-    public void preInitialization(FMLPreInitializationEvent preEvent) {        
-        PreInitializer.handle(preEvent);
-    }
+  public static final String NAME = "Craftable Enchantments";
+  public static final String ID = "craftenchants";
+
+  @Instance(ID)
+  public static CraftableEnchantments instance;
+
+  @SidedProxy(clientSide = Proxy.CLIENT, serverSide = Proxy.SERVER)
+  public static Proxy proxy;
+
+  @EventHandler
+  public void preInitialization(FMLPreInitializationEvent preEvent) {
+    PreInitializer.handle(preEvent);
+  }
+
+  @EventHandler
+  public void Initialization(FMLInitializationEvent initEvent) {
+    Initializer.handle(initEvent);
+  }
+
+  @EventHandler
+  public void postInitialization(FMLPostInitializationEvent postEvent) {
+    PostInitializer.handle(postEvent);
+  }
 }
