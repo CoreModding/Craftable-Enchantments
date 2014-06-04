270a4f72d88a8da1add4e48240f0483ca17b53b0
ShooShoSha <shooshosha@gmail.com>
2014-06-04 11:56:32 -0500

    Format file.

--- src/main/java/info/coremodding/craftenchants/item/ItemCE.java
+++ src/main/java/info/coremodding/craftenchants/item/ItemCE.java
@@ -1,8 +1,7 @@
 /**
-  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
-  * http://www.gnu.org/licenses/gpl.html
-  * http://www.gnu.org/licenses/lgpl.html
-  */
+ * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
+ * http://www.gnu.org/licenses/gpl.html http://www.gnu.org/licenses/lgpl.html
+ */
 package info.coremodding.craftenchants.item;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
@@ -14,36 +13,36 @@ import cpw.mods.fml.relauncher.SideOnly;
 import info.coremodding.craftenchants.item.enchants.Enchants;
 import info.coremodding.craftenchants.proxy.Proxy;
 
-public abstract class ItemCE extends Item implements Enchants {    
-    private Enchantment enchantType;
-    private int enchantLevel;
-    
-    protected ItemCE(String unlocalName) {
-        super();
-        setCreativeTab(Proxy.tabMain);
-        setUnlocalizedName(unlocalName);
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IIconRegister iconRegister) {
-        this.itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf('.') + 1));
-    }
-    
-    public boolean hasEnchantment() {
-        return getEnchantmentType() != null && getEnchantmentLevel() > 0;
-    }
-    
-    protected void setEnchantment(Enchantment type, int level) {
-      this.enchantType = type;
-      this.enchantLevel = level;
-    }
-    
-    public Enchantment getEnchantmentType() {
-        return this.enchantType;
-    }
-    
-    public int getEnchantmentLevel() {
-        return this.enchantLevel;
-    }
+public abstract class ItemCE extends Item implements Enchants {
+  private Enchantment enchantType;
+  private int enchantLevel;
+
+  protected ItemCE(String unlocalName) {
+    super();
+    setCreativeTab(Proxy.tabMain);
+    setUnlocalizedName(unlocalName);
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void registerIcons(IIconRegister iconRegister) {
+    this.itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf('.') + 1));
+  }
+
+  public boolean hasEnchantment() {
+    return getEnchantmentType() != null && getEnchantmentLevel() > 0;
+  }
+
+  protected void setEnchantment(Enchantment type, int level) {
+    this.enchantType = type;
+    this.enchantLevel = level;
+  }
+
+  public Enchantment getEnchantmentType() {
+    return this.enchantType;
+  }
+
+  public int getEnchantmentLevel() {
+    return this.enchantLevel;
+  }
 }
