35cefc6a41f24ecf10d8efc348fffb51e8818286
ShooShoSha <shooshosha@gmail.com>
2014-06-05 10:46:24 -0500

    Add pre-initialization methods.

--- src/main/java/info/coremodding/craftenchants/event/initialize/PreInitializer.java
+++ src/main/java/info/coremodding/craftenchants/event/initialize/PreInitializer.java
@@ -2,17 +2,18 @@
  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
  * http://www.gnu.org/licenses/gpl.html http://www.gnu.org/licenses/lgpl.html
  */
-package info.coremodding.craftenchants.event;
+package info.coremodding.craftenchants.event.initialize;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import info.coremodding.craftenchants.event.Events;
+import info.coremodding.craftenchants.item.ItemsCE;
 
 public final class PreInitializer {
 
   public static void handle(FMLPreInitializationEvent event) {
-    // TODO configuration handler
-    // TODO version handler
-    //PotionsCE.initialize();
-    
+    ItemsCE.initialize();
+    Events.initialize();
   }
 
 }
