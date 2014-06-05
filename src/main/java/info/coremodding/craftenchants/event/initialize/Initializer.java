4e65fcd777489af34b9c2fbb4ae24322e0e1ef18
ShooShoSha <shooshosha@gmail.com>
2014-06-05 10:45:22 -0500

    Move into own package.

--- src/main/java/info/coremodding/craftenchants/event/initialize/Initializer.java
+++ src/main/java/info/coremodding/craftenchants/event/initialize/Initializer.java
@@ -2,16 +2,14 @@
  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
  * http://www.gnu.org/licenses/gpl.html http://www.gnu.org/licenses/lgpl.html
  */
-package info.coremodding.craftenchants.event;
+package info.coremodding.craftenchants.event.initialize;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-import info.coremodding.craftenchants.item.ItemsCE;
 import info.coremodding.craftenchants.item.recipes.RecipesCE;
 
 public class Initializer {
   public static void handle(FMLInitializationEvent initEvent) {
-    ItemsCE.initialize();
     RecipesCE.initialize();
   }
 }
--- src/main/java/info/coremodding/craftenchants/event/initialize/PostInitializer.java
+++ src/main/java/info/coremodding/craftenchants/event/initialize/PostInitializer.java
@@ -2,14 +2,13 @@
  * Copyright 2014 by CoreModding under GNU Lesser General Public License (LGPLv3)
  * http://www.gnu.org/licenses/gpl.html http://www.gnu.org/licenses/lgpl.html
  */
-package info.coremodding.craftenchants.event;
+package info.coremodding.craftenchants.event.initialize;
 
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
 public class PostInitializer {
   public static void handle(FMLPostInitializationEvent postEvent) {
-    // TODO Implement method handle
-
+    
   }
 
 }
