0b832aa928d803440bd61fab695dcc97fe688b01
ShooShoSha <shooshosha@gmail.com>
2014-06-09 11:33:26 -0500

    Update item implementations.

--- src/main/java/info/coremodding/craftenchants/item/components/HorseHair.java
+++ src/main/java/info/coremodding/craftenchants/item/components/HorseHair.java
@@ -5,8 +5,7 @@ import info.coremodding.craftenchants.item.ItemCE;
 public class HorseHair extends ItemCE {
 
 	public HorseHair(int id) {
-		super(id);
-		setName("horsehair");
+		super(id, "horsehair");
 		setMaxStackSize(16);
 	}
 
